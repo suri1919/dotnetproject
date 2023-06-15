4021622ef30994a19ea066474ae849b18ebf80fb
suri1919 <126329472+suri1919@users.noreply.github.com>
2023-06-15 12:16:20 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 754495971822.dkr.ecr.ap-southeast-1.amazonaws.com
-docker pull 754495971822.dkr.ecr.ap-southeast-1.amazonaws.com/docker-project1:$1
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 754495971822.dkr.ecr.ap-south-1.amazonaws.com
+docker pull 754495971822.dkr.ecr.ap-south-1.amazonaws.com/docker-project1:latest:$1
 docker ps -a
 docker rm -f dotnet-app | ls -la
 docker ps -a
